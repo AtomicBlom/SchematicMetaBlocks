e03055658264de7517014a1c9cf2018e10934b39
Steven Blom <atomicblom@binaryvibrance.net>
2015-01-15 23:26:36 +1030

    Add support for not rendering unknown blocks.

--- src/main/java/net/binaryvibrance/schematicmetablocks/schematic/LoadSchematicCommand.java
+++ src/main/java/net/binaryvibrance/schematicmetablocks/schematic/LoadSchematicCommand.java
@@ -1,11 +1,14 @@
 package net.binaryvibrance.schematicmetablocks.schematic;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.ForgeDirection;
+import java.io.File;
 
 public class LoadSchematicCommand extends CommandBase
 {
@@ -25,14 +28,19 @@ public class LoadSchematicCommand extends CommandBase
     @Override
     public void processCommand(ICommandSender sender, String[] args)
     {
+
         if (sender instanceof EntityPlayerMP) {
+            if (args.length < 1) {
+                throw new CommandException("Could not execute command");
+            }
+            String filename = args[0];
+
             final EntityPlayerMP player = (EntityPlayerMP) sender;
             SchematicLoader loader = new SchematicLoader();
-            final ResourceLocation schematicLocation = new ResourceLocation("SteamNSteel:schematics/potato.schematic");
 
-            loader.loadSchematic(schematicLocation);
+            ResourceLocation schematicLocation = loader.loadSchematic(new File(Minecraft.getMinecraft().mcDataDir, "\\Schematics\\" + filename + ".schematic"));
             loader.renderSchematicInOneShot(schematicLocation, player.getEntityWorld(), (int) player.posX, (int) player.posY, (int) player.posZ, ForgeDirection.NORTH, false);
-            sender.addChatMessage(new ChatComponentText("Potato Spawned."));
+            sender.addChatMessage(new ChatComponentText("Rendered schematic " + filename));
         }
     }
 }
